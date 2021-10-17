7c3f8130cd00c7cd99111f1c6f02d6d16514afbc
Chris Ardon <rockrisbl@gmail.com>
2021-10-17 15:24:24 -0600

    login agregado

--- src/pantallas/PantallaAuth.js
+++ src/pantallas/PantallaAuth.js
@@ -1,14 +1,78 @@
-import React, {Component} from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import {FormControl, Stack, Input, NativeBaseProvider, Heading, Box, VStack, Link, Button, Text, HStack} from 'native-base'
 
- class PantallaAuth extends Component {
+
+
+class PantallaAuth extends Component {
     render() {
-        return (
-        <View>
-            <Text> Pantalla Auth </Text>
-            </View>
-            );
-        }
-    }
+        return(
+            <NativeBaseProvider>
+                <Box safeArea flex={1} p='2' py='20' w='90%' mx='auto' >
+                    <Heading size='lg' fontWeight='600' color='coolGray.800'>
+                        Bienvenido!
+                    </Heading>
+                    <Heading mt='1' color='coolGray.600' fontWeight='medium' size='xs'>
+                        Ingresa para continuar
+                    </Heading>
 
-    export default PantallaAuth; 
+                    <VStack space={3} mt='5'>
+                        <FormControl>
+                            <FormControl.Label
+                            _text={{
+                                color: 'coolGray.800',
+                                fontSize:15,
+                                fontWeight:500,
+                            }}>
+                                Email
+                            </FormControl.Label>
+                            <Input></Input>
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label
+                            _text={{
+                                color: 'coolGray.800',
+                                fontSize:15,
+                                fontWeight:500
+                            }}>
+                                Contraseña
+                            </FormControl.Label>
+                            <Input type='password'></Input>
+                            <Link
+                            _text={{
+                                fontSize:'xs', 
+                                fontWeight:'500',
+                                color:'indigo.500'
+                            }} alignSelf='flex-end' mt='1'>
+                                Olvidaste tu contraseña?
+                            </Link>
+                        </FormControl>
+                        <Button mt='2' colorScheme="indigo" _text={{ color:'white' }}>
+                            Entrar
+                        </Button>
+                        <HStack mt='6' justifyContent='center'>
+                            <Text fontSize='sm' color='muted.700' fontWeight={400}>
+                                Soy un nuevo usuario.{' '}
+                            </Text>
+                            <Link _text={{
+                                color:'indigo.500',
+                                fontWeight:'medium',
+                                fontSize:'sm',
+                            }} href='#'>
+                                Registrarme
+                            </Link>
+                        </HStack>
+                    </VStack>
+                </Box>
+            </NativeBaseProvider>
+        );
+    }
+}
+
+export default PantallaAuth;
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'red',
+    },
+})
