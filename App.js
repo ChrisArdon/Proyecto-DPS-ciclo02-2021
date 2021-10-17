0558f4b8da732eb2a294734559c5d21629a14c9f
Andrea-EH <andrea.henriquezz20@gmail.com>
2021-10-16 22:09:38 -0600

    Splash agregado

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import React from "react";
+
+import React, {Component} from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
-import Navegacion from "./src/navegacion/Navegacion";
+import {Router, Scene} from 'react-native-router-flux'
+/**
+ * Componentes para rutas
+ */
 
-const App = () => {
-  return(
-    <NavigationContainer>
-      <Navegacion></Navegacion>
-    </NavigationContainer>
-  );
-}
+import Pantalladecarga from "./src/pantallas/Pantalladecarga";
+import PantallaAuth from "./src/pantallas/PantallaAuth";
+import Navegacion from "./src/navegacion/Navegacion";
+import react from "react";
+
+class App extends Component {
+  render() {
+      return (
+        
+      <Router>
+        <Scene key="root">
+          <Scene key="cargando" component={Pantalladecarga} initial={true} hideNavBar>  </Scene>
+          <Scene key="auth" component={PantallaAuth} hideNavBar={true}> </Scene>
+          </Scene>
+          </Router>
+          
+          );
+      }
+      
+  }
+// const App = () => {
+//   return(
+//     <NavigationContainer>
+//       <Navegacion></Navegacion>
+//     </NavigationContainer>
+//   );
+// }
 
 export default App;
---
+++ src/pantallas/PantallaAuth.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+ class PantallaAuth extends Component {
+    render() {
+        return (
+        <View>
+            <Text> Pantalla Auth </Text>
+            </View>
+            );
+        }
+    }
+
+    export default PantallaAuth; 
